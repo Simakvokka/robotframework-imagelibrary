764e3b1fb1a089ac71fe5d4658b8bd94f41352a7
AxelC <axel.catoire@gmail.com>
2021-12-14 14:36:30 +0100

    Correct POINT usage which seem not to be in "pyautogui._pyautogui_win…" anymore.

--- src/ImageLibrary/GUIProcess/_gui_win.py
+++ src/ImageLibrary/GUIProcess/_gui_win.py
@@ -1,13 +1,15 @@
 import ctypes
 import ctypes.wintypes
-from pyautogui._pyautogui_win import POINT
+from pyautogui import Point
 
 
 class RECT (ctypes.Structure):
-    _fields_ = [ ("left", ctypes.wintypes.LONG),
-                 ("top",  ctypes.wintypes.LONG),
-                 ("right", ctypes.wintypes.LONG),
-                 ("bottom", ctypes.wintypes.LONG)]
+    _fields_ = [
+        ("left", ctypes.wintypes.LONG),
+        ("top",  ctypes.wintypes.LONG),
+        ("right", ctypes.wintypes.LONG),
+        ("bottom", ctypes.wintypes.LONG)
+    ]
 
 
 def get_window_pid(hwnd):
@@ -19,19 +21,17 @@ def get_window_pid(hwnd):
 def get_active_window():
     return ctypes.windll.user32.GetForegroundWindow()
 
+
 def get_window_client_rect(hwnd):
     rc = RECT()
     ctypes.windll.user32.GetClientRect(hwnd, ctypes.byref(rc))
 
-    pt = POINT()
-    pt.x = rc.left
-    pt.y = rc.top
+    pt = Point(rc.left, rc.top)
     ctypes.windll.user32.ClientToScreen(hwnd, ctypes.byref(pt))
 
     return pt.x, pt.y, rc.right - rc.left, rc.bottom - rc.top
 
 
 def set_active_window(hwnd):
-    #ctypes.windll.user32.SetFocus(hwnd)
+    # ctypes.windll.user32.SetFocus(hwnd)
     ctypes.windll.user32.SetForegroundWindow(hwnd)
-
